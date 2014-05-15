9052770c7cbbf6859a754df283bfcac36af6c379
Alessandro Stein <alessandro@alessandrostein.com>
2014-05-14 23:05:50 -0300

    Init projects

---
+++ SorteadorEnterprise/SorteadorEnterprise-ejb/src/java/java/sorteador/ejbs/SorteadorBean.java
@@ -0,0 +1,34 @@
+package java.sorteador.ejbs;
+
+import java.util.Random;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author alessandro
+ */
+@Stateless
+@Remote(SorteadorBeanRemote.class)
+public class SorteadorBean implements SorteadorBeanRemote {
+
+     private final Random gerador = new Random();
+
+    @Override
+    public int sortear() {
+        System.out.println("SorteadorBean:sortear() foi invocado...");
+        return this.gerador.nextInt(6) + 1;
+    }
+    
+    @PostConstruct
+    public void inicializando() {
+        System.out.println("Mais uma SorteadorBean criado...");
+    }
+
+    @PreDestroy
+    public void destruindo() {
+        System.out.println("Mais uma SorteadorBean será destruído...");
+    }    
+}
