e0ebb89b02bd5db34bba3f4c1142469c25e68b3d
Alessandro Stein <alessandro@alessandrostein.com>
2014-05-15 21:43:31 -0300

    Add User

---
+++ SorteadorInterface/src/br/org/catolicasc/sorteador/interfaces/UserFacadeRemote.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.org.catolicasc.sorteador.interfaces;
+
+import br.org.catolicasc.sorteador.entity.User;
+import java.util.List;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author alessandro
+ */
+@Remote
+public interface UserFacadeRemote {
+
+    void create(User user);
+
+    void edit(User user);
+
+    void remove(User user);
+
+    User find(Object id);
+
+    List<User> findAll();
+
+    List<User> findRange(int[] range);
+
+    int count();
+    
+}
